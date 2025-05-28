ba085ca17b9f7db837745566908efd76c6f58084
LZ <lucas.mfaiad@gmail.com>
2025-05-27 23:57:05 -0700

    Java Script Classes

---
+++ Java_Script/script.js
@@ -0,0 +1,6 @@
+// document.write("Whatever")
+// document.getElementById("placeholder1").innerHTML = "Hello, World!";
+// window.alert("Whatever");
+document.write("hello world");
+document.getElementById("placeholder1").innerHTML = 4+6; 
+window.alert("Hello, World!");
