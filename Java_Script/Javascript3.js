6657f2f6e5e6a1f98a8622511dae13f9a87627c0
LZ <lucas.mfaiad@gmail.com>
2025-05-30 10:19:43 -0700

    JavaScript Studies

---
+++ Java_Script/Javascript3.js
@@ -0,0 +1,6 @@
+function displayFirstInput() {
+    document.getElementById("output1").innerHTML = document.getElementById("input1").value;
+}
+function displaySecondInput() {
+    document.getElementById("output2").innerHTML = document.getElementById("input2").value;
+}
--- Java_Script/script.js
+++ Java_Script/script.js
@@ -4,3 +4,18 @@
 document.write("hello world");
 document.getElementById("placeholder1").innerHTML = 4+6; 
 window.alert("Hello, World!");
+function fillWithQuerySelector() {
+    document.querySelector("#querySelectorTarget").innerHTML = "Selected with querySelector!";
+}
+
+function fillWithGetElementById() {
+    document.getElementById("getElementByIdTarget").innerHTML = "Selected with getElementById!";
+}
+
+function fillWithGetElementsByClassName() {
+    document.getElementsByClassName("classTarget")[0].innerHTML = "Selected with getElementsByClassName!";
+}
+
+function fillWithGetElementsByTagName() {
+    document.getElementsByTagName('span')[0].innerHTML = "Selected with getElementsByTagName!";
+}
